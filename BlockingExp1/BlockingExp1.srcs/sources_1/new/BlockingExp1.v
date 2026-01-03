7cfecd1d179881d26c4de5b7df4c28fc118baf7b
Denny <dennythenguyen@gmail.com>
2026-01-02 17:35:44 -0800

    Add files via upload

---
+++ BlockingExp1/BlockingExp1.srcs/sources_1/new/BlockingExp1.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module BlockingExp1(D, Clock, Q1, Q2);
+    input D, Clock;
+    output reg Q1, Q2;
+    always @(posedge Clock)
+        begin
+            Q1 <= D;
+            Q2 <= Q1;
+    end
+endmodule
